80495ef564ac3af02b1cf290c5104d76db755d57
Raphael Panta <raphaelpanta@gmail.com>
2018-01-13 11:34:03 -0800

    creating unit test

--- api/animals/test_endpoints.py
+++ api/animals/test_endpoints.py
@@ -1,5 +1,13 @@
+from datetime import datetime
+from json import loads
+from .model import Animal
 from .endpoints import animals
 
 
 def test_animals():
-    assert animals() == '"Hello animals world!"'
+    d1 = Animal(name='Dog 1', age=64, breed = ['lontras'], arrived_date = datetime(2018,1,1), description ="um cachorro do barulho")
+    d1.save()
+    rs = loads(animals())
+    assert len(rs)==1
+    assert rs[0]['id']== str(d1.id)
+
